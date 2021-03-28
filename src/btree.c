f29fbc311ea9a24658f3b7885ee0cd59e8d7a56a
Plisp <tianlinqu@gmail.com>
2021-03-28 11:07:51 +1100

    basic btree works and performs. Yet to adapt structure

--- src/btree.c
+++ src/btree.c
@@ -29,7 +29,7 @@ struct piece {
 #define B (NODESIZE / PER_B)
 struct inner {
 	long bytes[B];
-	struct node *children[B];
+	struct inner *children[B];
 };
 
 #define PER_BL (sizeof(struct piece) + sizeof(long))
@@ -85,7 +85,7 @@ static void *bval(unsigned level, struct inner *node, unsigned n)
 		// N.B. returns NULL on absent value
 		return piece->len > 0 ? piece : NULL;
 	} else
-		return &node->children[n];
+		return node->children[n];
 }
 
 static void setval(unsigned level, struct inner *node, unsigned n, void *val)
@@ -110,6 +110,145 @@ static void clearpair(unsigned level, struct inner *node, unsigned n)
 		node->children[n] = 0;
 	}
 }
+
+/* debug */
+
+void pt_print_struct_sizes(void)
+{
+	fprintf(stderr,
+		"Implementation: \e[38;5;1mbtree\e[0m\n"
+		"sizeof(struct inner): %zd\n"
+		"sizeof(struct leaf): %zd\n"
+		"sizeof(PieceTable): %zd\n",
+		sizeof(struct inner), sizeof(struct leaf), sizeof(PieceTable));
+}
+
+static void print_node(unsigned level, struct inner *node)
+{
+	char out[256], *it = out;
+	if(level == 1) {
+		it += sprintf(it, "[k: ");
+		for(unsigned i = 0; i < BL; i++)
+			it += sprintf(it, "%lu|", bkey(1, node, i));
+		it--;
+		it += sprintf(it, " p: ");
+		for(unsigned i = 0; i < BL; i++) {
+			struct piece *val = bval(1, node, i);
+			it += sprintf(it, "%lu|", val ? val->len : 0);
+		}
+		it--;
+		it += sprintf(it, "]");
+	} else {
+		printf("[k: ");
+		for(unsigned i = 0; i < B; i++)
+			it += sprintf(it, "%lu|", bkey(level, node, i));
+		it += sprintf(it, "]");
+	}
+	fprintf(stderr, "%s\n", out);
+}
+
+// TODO this probably won't work well
+void pt_pprint(PieceTable *pt)
+{
+	struct inner *root = pt->root;
+	print_node(pt->height, root);
+}
+
+#include "dot.h"
+
+#define FSTR(dest, str, ...)                                                   \
+	do {                                                                   \
+		int len = snprintf(NULL, 0, str, __VA_ARGS__);                 \
+		dest = realloc(dest, len + 1);                                 \
+		sprintf(dest, str, __VA_ARGS__);                               \
+	} while(0)
+
+void piece_to_dot(FILE *file, const struct piece *piece)
+{
+	char *tmp = NULL, *port = NULL;
+	if(!piece) {
+		graph_table_entry(file, NULL, NULL);
+		return;
+	}
+	FSTR(tmp, "len: %lu", piece->len);
+	FSTR(port, "%lu", (long)piece->blk);
+	graph_table_entry(file, tmp, port);
+	// TODO link to block
+	//graph_link(file, piece, tmp, piece->blk, "body");
+	free(tmp);
+	free(port);
+}
+
+void leaf_to_dot(FILE *file, const struct leaf *leaf)
+{
+	char *tmp = NULL;
+	graph_table_begin(file, leaf, "aquamarine3");
+	for(unsigned i = 0; i < BL; i++) {
+		FSTR(tmp, "%lu", bkey(1, (void *)leaf, i));
+		graph_table_entry(file, tmp, NULL);
+	}
+	for(unsigned i = 0; i < BL; i++)
+		piece_to_dot(file, bval(1, (void *)leaf, i));
+	graph_table_end(file);
+	free(tmp);
+}
+
+void inner_to_dot(FILE *file, const struct inner *root, unsigned height)
+{
+	if(!root)
+		return;
+	if(height == 1)
+		return leaf_to_dot(file, (void *)root);
+	char *tmp = NULL, *port = NULL;
+	graph_table_begin(file, root, NULL);
+	for(unsigned i = 0; i < B; i++) {
+		FSTR(tmp, "%lu", bkey(height, (void *)root, i));
+		FSTR(port, "%u", i);
+		graph_table_entry(file, tmp, port);
+	}
+	graph_table_end(file);
+	// output child links
+	for(unsigned i = 0; i < B; i++) {
+		struct inner *child = bval(height, (void *)root, i);
+		if(!child)
+			break;
+		FSTR(tmp, "%d", i);
+		graph_link(file, root, tmp, child, "body");
+		inner_to_dot(file, child, height - 1);
+	}
+	free(tmp);
+	free(port);
+}
+
+bool pt_to_dot(PieceTable *pt, const char *path)
+{
+	FILE *file = fopen(path, "w");
+	char *tmp = NULL;
+	if(!file)
+		goto fail;
+
+	graph_begin(file);
+	graph_table_begin(file, pt, NULL);
+	FSTR(tmp, "height: %u", pt->height);
+	graph_table_entry(file, tmp, NULL);
+	graph_table_entry(file, "root", "root");
+	graph_table_end(file);
+	graph_link(file, pt, "root", pt->root, "body");
+	if(pt->root)
+		inner_to_dot(file, pt->root, pt->height);
+	graph_end(file);
+
+	free(tmp);
+	if(!fclose(file))
+		goto fail;
+	return true;
+
+fail:
+	perror("pt_to_dot");
+	return false;
+}
+
+/* basic */
 
 struct PieceTable *btree_new(void)
 {
@@ -153,6 +292,7 @@ void *btree_lookup(struct PieceTable *pt, unsigned long key)
 	return NULL;
 }
 
+#if 0
 /*
  * Usually this function is quite similar to normal lookup.  But the key of
  * a parent node may be smaller than the smallest key of all its siblings.
@@ -209,7 +349,10 @@ miss:
 	}
 	return NULL;
 }
+#endif
 
+// returns index of the first key spanning the search key
+// the key is the smallest value of the corresponding span (kprev,k]
 static unsigned getpos(unsigned level, struct inner *node, unsigned long key)
 {
 	unsigned i;
@@ -219,6 +362,7 @@ static unsigned getpos(unsigned level, struct inner *node, unsigned long key)
 	return i;
 }
 
+// count the number of live entries starting from START as an optimization
 static unsigned getfill(unsigned level, struct inner *node, int start)
 {
 	unsigned i;
@@ -256,6 +400,8 @@ static struct inner *find_level(struct PieceTable *pt, unsigned long key,
 	return node;
 }
 
+/* insertion */
+
 static int btree_grow(struct PieceTable *pt)
 {
 	struct inner *node = btree_node_alloc(pt->height + 1);
@@ -270,27 +416,17 @@ static int btree_grow(struct PieceTable *pt)
 	return 0;
 }
 
-static void btree_shrink(struct PieceTable *pt)
-{
-	if(pt->height <= 1)
-		return;
-	struct inner *node = pt->root;
-	int fill = getfill(pt->height, node, 0);
-	assert(fill <= 1);
-	pt->root = bval(pt->height, node, 0);
-	pt->height--;
-	free(node);
-}
-
-static void print_node(unsigned level, struct inner *node);
 static void btree_insert_level(struct PieceTable *pt, unsigned long key,
 			       void *val, unsigned level)
 {
 	struct inner *node;
 	unsigned i, pos, fill;
 
-	if(pt->height < level)
+	if(pt->height < level) {
 		btree_grow(pt);
+		pt_dbg("expanding for insertng %lu\n", key);
+		//pt_to_dot(pt, "p.dot");
+	}
 
 retry:
 	node = find_level(pt, key, level);
@@ -298,7 +434,7 @@ retry:
 	fill = getfill(level, node, pos);
 	pt_dbg("fill: %u at level: %u\n", fill, level);
 	/* check for node split */
-	if(fill == B) {
+	if(fill == (level == 1 ? BL : B)) {
 		struct inner *new = btree_node_alloc(level);
 		btree_insert_level(pt, bkey(level, node, fill / 2 - 1), new,
 				   level + 1);
@@ -310,9 +446,15 @@ retry:
 			setval(level, node, i, bval(level, node, i + fill / 2));
 			clearpair(level, node, i + fill / 2);
 		}
+		// copy final odd entry
+		if(fill & 1) {
+			setkey(level, node, i, bkey(level, node, fill - 1));
+			setval(level, node, i, bval(level, node, fill - 1));
+			clearpair(level, node, fill - 1);
+		}
 		goto retry;
 	}
-	assert(fill < B);
+	assert(fill < (level == 1 ? BL : B));
 	/* shift and insert */
 	for(i = fill; i > pos; i--) {
 		setkey(level, node, i, bkey(level, node, i - 1));
@@ -326,6 +468,20 @@ void btree_insert(struct PieceTable *pt, unsigned long key, void *val)
 {
 	assert(val); // required since we null empty entries instead of storing sizes
 	btree_insert_level(pt, key, val, 1);
+}
+
+/* deletion */
+
+static void btree_shrink(struct PieceTable *pt)
+{
+	if(pt->height <= 1)
+		return;
+	struct inner *node = pt->root;
+	int fill = getfill(pt->height, node, 0);
+	assert(fill <= 1);
+	pt->root = bval(pt->height, node, 0);
+	pt->height--;
+	free(node);
 }
 
 static void *btree_remove_level(struct PieceTable *pt, unsigned long key,
@@ -432,154 +588,28 @@ void *btree_remove(struct PieceTable *pt, unsigned long key)
 	return btree_remove_level(pt, key, 1);
 }
 
-void pt_print_struct_sizes(void)
-{
-	fprintf(stderr,
-		"Implementation: \e[38;5;1mbtree\e[0m\n"
-		"sizeof(struct inner): %zd\n"
-		"sizeof(struct leaf): %zd\n"
-		"sizeof(PieceTable): %zd\n",
-		sizeof(struct inner), sizeof(struct leaf), sizeof(PieceTable));
-}
-
-static void print_node(unsigned level, struct inner *node)
-{
-	char out[256], *it = out;
-	if(level == 1) {
-		it += sprintf(it, "[k: ");
-		for(unsigned i = 0; i < BL; i++)
-			it += sprintf(it, "%lu|", bkey(1, node, i));
-		it--;
-		it += sprintf(it, " p: ");
-		for(unsigned i = 0; i < BL; i++) {
-			struct piece *val = bval(1, node, i);
-			it += sprintf(it, "%lu|", val ? val->len : 0);
-		}
-		it--;
-		it += sprintf(it, "]");
-	} else {
-		printf("[k: ");
-		for(unsigned i = 0; i < B; i++)
-			it += sprintf(it, "%lu|", bkey(level, node, i));
-		it += sprintf(it, "]");
-	}
-	fprintf(stderr, "%s\n", out);
-}
-
-// TODO this probably won't work well
-void pt_pprint(PieceTable *pt)
-{
-	struct inner *root = pt->root;
-	print_node(pt->height, root);
-}
-
-/* dot output */
-
-#include "dot.h"
-
-#define FSTR(dest, str, ...)                                                   \
-	do {                                                                   \
-		int len = snprintf(NULL, 0, str, __VA_ARGS__);                 \
-		dest = realloc(dest, len + 1);                                 \
-		sprintf(dest, str, __VA_ARGS__);                               \
-	} while(0)
-
-void piece_to_dot(FILE *file, const struct piece *piece)
-{
-	if(!piece)
-		return;
-	char *tmp = NULL, *port = NULL;
-	FSTR(tmp, "len: %lu", piece->len);
-	FSTR(port, "%lu", (long)piece->blk);
-	graph_table_entry(file, tmp, port);
-	// TODO link to block
-	//graph_link(file, piece, tmp, piece->blk, "body");
-	free(tmp);
-}
-
-void leaf_to_dot(FILE *file, const struct leaf *leaf)
-{
-	char *tmp = NULL;
-	graph_table_begin(file, leaf, "aquamarine3");
-	for(unsigned i = 0; i < BL; i++) {
-		FSTR(tmp, "%lu", bkey(1, (void *)leaf, i));
-		graph_table_entry(file, tmp, NULL);
-	}
-	for(unsigned i = 0; i < BL; i++)
-		piece_to_dot(file, bval(1, (void *)leaf, i));
-	graph_table_end(file);
-	free(tmp);
-}
-
-void inner_to_dot(FILE *file, const struct inner *root, unsigned height)
-{
-	if(!root) 
-		return;
-	if(height == 1)
-		return leaf_to_dot(file, (void *)root);
-	char *tmp = NULL;
-	graph_table_begin(file, root, NULL);
-	for(unsigned i = 0; i < B; i++) {
-		FSTR(tmp, "%lu", bkey(height, (void *)root, i));
-		graph_table_entry(file, tmp, tmp);
-	}
-	graph_table_end(file);
-	// output child links
-	for(unsigned i = 0; i < B; i++) {
-		struct inner *child = bval(height, (void *)root, i);
-		if(!child)
-			break;
-		FSTR(tmp, "%d", i);
-		graph_link(file, root, tmp, child, "body");
-		inner_to_dot(file, child, height - 1);
-	}
-	free(tmp);
-}
-
-bool pt_to_dot(PieceTable *pt, const char *path)
-{
-	FILE *file = fopen(path, "w");
-	char *tmp = NULL;
-	if(!file)
-		goto fail;
-
-	graph_begin(file);
-	graph_table_begin(file, pt, NULL);
-	FSTR(tmp, "height: %u", pt->height);
-	graph_table_entry(file, tmp, NULL);
-	graph_table_entry(file, "root", "root");
-	graph_table_end(file);
-	graph_link(file, pt, "root", pt->root, "body");
-	if(pt->root)
-		inner_to_dot(file, pt->root, pt->height);
-	graph_end(file);
-
-	free(tmp);
-	if(!fclose(file))
-		goto fail;
-	return true;
-
-fail:
-	perror("pt_to_dot");
-	return false;
-}
-
 int main(void)
 {
 	pt_print_struct_sizes();
 	struct PieceTable *pt = btree_new();
-	struct piece p = (struct piece){ .len = 42 };
+	struct piece p = (struct piece){ .len = 1 };
+	/*
 	btree_insert(pt, 42, &p);
 	p.len++;
 	btree_insert(pt, 12, &p);
 	p.len++;
 	btree_insert(pt, 21, &p);
-	//btree_insert(pt, 24, &i);
-	pt_to_dot(pt, "t.dot");
-	/*
-	for(int i = 0; i < 100000; i += 2) {
-		btree_insert(b, i, (void *)1);
-		btree_insert(b, i+1, (void *)1);
-	}
+	p.len++;
+	btree_insert(pt, 24, &p);
+	p.len++;
+	btree_insert(pt, 1, &p);
+	p.len++;
+	btree_insert(pt, 36, &p);
 	*/
+	for(int i = 0; i < 100; i += 2) {
+		btree_insert(pt, i, &p);
+		btree_insert(pt, i+1, &p);
+	}
+	pt_to_dot(pt, "t.dot");
+	//printf("bytes[0]: %ld\n", pt->root->children[0]->children[0]->bytes[0]);
 }
